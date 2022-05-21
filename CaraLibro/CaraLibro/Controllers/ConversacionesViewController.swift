4554aa18ffaf5276cb21b88d099ec4a8d61d9a9f
user206601 <user206601@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2022-05-21 00:43:28 -0400

    RegistroViewController-20/05/2022

---
+++ CaraLibro/CaraLibro/Controllers/ConversacionesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaraLibro
+//
+//  Created by user206601 on 5/20/22.
+//
+
+import UIKit
+
+class ConversacionesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
